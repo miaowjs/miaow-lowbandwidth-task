12ccb647a96bc48b1bda2c706b7138a6fc76185d
anhulife <anhulife@gmail.com>
2016-02-01 17:40:36 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,156 @@
+var _ = require('lodash');
+var async = require('async');
+var gm = require('gm');
+var path = require('path');
+var postcss = require('postcss');
+
+var pkg = require('./package.json');
+
+function getBackgroundImageAndNodeList(context, root, callback) {
+  var backgroundImageList = [];
+
+  var reg = new RegExp('url\\s*\\(\\s*[\'"]?([^\/][\\w\\_\\/\\.\\-]*)[\'\"]?\\s*\\)', 'im');
+
+  var backgroundNodeList = _.filter(root.nodes, function(node) {
+    if (node.type === 'rule') {
+      return _.findLast(node.nodes, function(subNode) {
+        if (
+          subNode.type === 'decl' &&
+          (subNode.prop === 'background' || subNode.prop === 'background-image') &&
+          reg.test(subNode.value)
+        ) {
+          backgroundImageList.push({
+            src: reg.exec(subNode.value)[1]
+          });
+          return true;
+        }
+      });
+    }
+  });
+
+  callback(null, backgroundImageList, backgroundNodeList);
+}
+
+function getImageInfo(context, backgroundImageList, backgroundNodeList, callback) {
+  async.eachSeries(
+    backgroundImageList,
+    function(backgroundImage, callback) {
+      async.series([
+        function(callback) {
+          context.resolveModule(backgroundImage.src, function(err, module) {
+            if (err) {
+              return callback(err);
+            }
+
+            backgroundImage.url = module.url;
+            backgroundImage.src = module.src;
+            callback();
+          });
+        },
+
+        function(callback) {
+          gm(path.resolve(context.context, backgroundImage.src))
+            .identify(function(err, data) {
+              if (err) {
+                return callback(err);
+              }
+
+              backgroundImage.width = data.size.width;
+              backgroundImage.height = data.size.height;
+              backgroundImage.format = data.format;
+
+              callback();
+            });
+        }
+      ], callback);
+    },
+
+    function(err) {
+      callback(err, backgroundImageList, backgroundNodeList);
+    });
+}
+
+function scaleImage(context, backgroundImageList, backgroundNodeList, callback) {
+  async.eachSeries(
+    backgroundImageList,
+    function(backgroundImage, callback) {
+      var width = backgroundImage.width / 2;
+      var height = backgroundImage.height / 2;
+
+      gm(path.resolve(context.context, backgroundImage.src))
+        .resize(width, height)
+        .toBuffer(backgroundImage.format, function (err, contents) {
+          if (err) {
+            return callback(err);
+          }
+
+          var src = backgroundImage.src;
+          var extname = path.extname(src);
+
+          src = src.replace(new RegExp(extname + '$'), '.lowbandwidth' + extname);
+
+          context.emitModule(src, contents, function(err, module) {
+            if (err) {
+              return callback(err);
+            }
+
+            backgroundImage.scaled = module.url;
+            callback();
+          });
+        });
+    },
+
+    function(err) {
+      callback(err, backgroundImageList, backgroundNodeList);
+    });
+}
+
+function addRule(context, root, backgroundImageList, backgroundNodeList, callback) {
+  // 追加1倍图规则
+  _.each(backgroundNodeList, function(backgroundNode, index) {
+    var rule = '{background-image: url('+ backgroundImageList[index].scaled +');}';
+    var selectors = backgroundNode.selectors.map(function(selector) {
+      return '.lowbandwidth ' + selector;
+    });
+
+    root.nodes.splice(root.nodes.indexOf(backgroundNode) + 1, 0, postcss.parse(selectors + rule).first);
+  });
+
+  callback();
+}
+
+module.exports = function(options, callback) {
+  var context = this;
+
+  // 样式语法树根
+  var root = postcss.parse(
+    context.contents.toString(),
+    {from: path.resolve(context.context, context.src)}
+  );
+
+  async.waterfall(
+    [
+      _.partial(getBackgroundImageAndNodeList, context, root),
+      _.partial(getImageInfo, context),
+      _.partial(scaleImage, context),
+      _.partial(addRule, context, root)
+    ],
+    function(err) {
+      if (err) {
+        return callback(err);
+      }
+
+      try {
+        context.contents = new Buffer(root.toResult().css);
+      } catch (err) {
+        return callback(err);
+      }
+
+      callback();
+    }
+  );
+};
+
+module.exports.toString = function() {
+  return [pkg.name, pkg.version].join('@');
+};
---
+++ test/test.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+var fs = require('fs');
+var miaow = require('miaow');
+var path = require('path');
+
+var parse = require('..');
+describe('miaow-lowbandwidth-task', function() {
+  this.timeout(10e3);
+
+  var log;
+
+  function doCompile(done) {
+    miaow({
+      context: path.resolve(__dirname, './fixtures')
+    }, function(err) {
+      if (err) {
+        console.error(err.toString(), err.stack);
+        process.exit(1);
+      }
+
+      log = JSON.parse(fs.readFileSync(path.resolve(__dirname, './output/miaow.log.json')));
+      done();
+    });
+  }
+
+  before(doCompile);
+
+  it('接口是否存在', function() {
+    assert(!!parse);
+  });
+
+  it('修改样式', function() {
+    assert.equal(log.modules['foo.css'].destHash, 'f572752b82eed04247f8bed0811f537a');
+  });
+
+  it('生成低流量图片', function() {
+    assert.equal(log.modules['foo.lowbandwidth.png'].destHash, 'f4e73883ae35fd2d63ec352cf222e74d');
+  });
+});
