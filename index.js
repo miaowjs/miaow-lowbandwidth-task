cee237b89c3752948228fcd5722ac9276e02e802
anhulife <anhulife@gmail.com>
2016-02-24 14:22:38 +0800

    将获取低带宽图片的处理过程独立出去

--- index.js
+++ index.js
@@ -1,123 +1,13 @@
 var _ = require('lodash');
 var async = require('async');
-var gm = require('gm');
 var path = require('path');
 var postcss = require('postcss');
 
+var getLowbandwidthImage = require('./lib/getLowbandwidthImage');
+
 var pkg = require('./package.json');
 
-function getBackgroundImageAndNodeList(context, root, callback) {
-  var backgroundImageList = [];
-
-  var reg = new RegExp('url\\s*\\(\\s*[\'"]?([^\/][\\w\\_\\/\\.\\-]*)[\'\"]?\\s*\\)', 'im');
-
-  var backgroundNodeList = _.filter(root.nodes, function(node) {
-    if (node.type === 'rule') {
-      return _.findLast(node.nodes, function(subNode) {
-        if (
-          subNode.type === 'decl' &&
-          (subNode.prop === 'background' || subNode.prop === 'background-image') &&
-          reg.test(subNode.value)
-        ) {
-          backgroundImageList.push({
-            src: reg.exec(subNode.value)[1]
-          });
-          return true;
-        }
-      });
-    }
-  });
-
-  callback(null, backgroundImageList, backgroundNodeList);
-}
-
-function getImageInfo(context, backgroundImageList, backgroundNodeList, callback) {
-  async.eachSeries(
-    backgroundImageList,
-    function(backgroundImage, callback) {
-      async.series([
-        function(callback) {
-          context.resolveModule(backgroundImage.src, function(err, module) {
-            if (err) {
-              return callback(err);
-            }
-
-            backgroundImage.url = module.url;
-            backgroundImage.src = module.src;
-            callback();
-          });
-        },
-
-        function(callback) {
-          gm(path.resolve(context.context, backgroundImage.src))
-            .identify(function(err, data) {
-              if (err) {
-                return callback(err);
-              }
-
-              backgroundImage.width = data.size.width;
-              backgroundImage.height = data.size.height;
-              backgroundImage.format = data.format;
-
-              callback();
-            });
-        }
-      ], callback);
-    },
-
-    function(err) {
-      callback(err, backgroundImageList, backgroundNodeList);
-    });
-}
-
-function scaleImage(context, backgroundImageList, backgroundNodeList, callback) {
-  async.eachSeries(
-    backgroundImageList,
-    function(backgroundImage, callback) {
-      var width = backgroundImage.width / 2;
-      var height = backgroundImage.height / 2;
-
-      gm(path.resolve(context.context, backgroundImage.src))
-        .resize(width, height)
-        .toBuffer(backgroundImage.format, function (err, contents) {
-          if (err) {
-            return callback(err);
-          }
-
-          var src = backgroundImage.src;
-          var extname = path.extname(src);
-
-          src = src.replace(new RegExp(extname + '$'), '.lowbandwidth' + extname);
-
-          context.emitModule(src, contents, function(err, module) {
-            if (err) {
-              return callback(err);
-            }
-
-            backgroundImage.scaled = module.url;
-            callback();
-          });
-        });
-    },
-
-    function(err) {
-      callback(err, backgroundImageList, backgroundNodeList);
-    });
-}
-
-function addRule(context, root, backgroundImageList, backgroundNodeList, callback) {
-  // 追加1倍图规则
-  _.each(backgroundNodeList, function(backgroundNode, index) {
-    var rule = '{background-image: url('+ backgroundImageList[index].scaled +');}';
-    var selectors = backgroundNode.selectors.map(function(selector) {
-      return '.lowbandwidth ' + selector;
-    });
-
-    root.nodes.splice(root.nodes.indexOf(backgroundNode) + 1, 0, postcss.parse(selectors + rule).first);
-  });
-
-  callback();
-}
+var reg = /url\s*\(\s*['"]?([^\/][\w_\/\.\-]*)['"]?\)/;
 
 module.exports = function(options, callback) {
   var context = this;
@@ -128,17 +18,49 @@ module.exports = function(options, callback) {
     {from: path.resolve(context.context, context.src)}
   );
 
-  async.waterfall(
-    [
-      _.partial(getBackgroundImageAndNodeList, context, root),
-      _.partial(getImageInfo, context),
-      _.partial(scaleImage, context),
-      _.partial(addRule, context, root)
-    ],
-    function(err) {
+  var backgroundImageList = [];
+
+  // 获取带有背景图设置的节点
+  var backgroundNodeList = _.filter(root.nodes, function(node) {
+    if (node.type === 'rule') {
+      return _.findLast(node.nodes, function(subNode) {
+        if (
+          subNode.type === 'decl' &&
+          (subNode.prop === 'background' || subNode.prop === 'background-image') &&
+          reg.test(subNode.value)
+        ) {
+          backgroundImageList.push(reg.exec(subNode.value)[1]);
+          return true;
+        }
+      });
+    }
+  });
+
+  if (backgroundNodeList.length === 0) {
+    return callback();
+  }
+
+  // 追加1倍图规则
+  async.mapSeries(
+    backgroundImageList,
+    getLowbandwidthImage.bind(context),
+    function(err, lowbandwidthImageList){
       if (err) {
         return callback(err);
       }
+
+      _.each(backgroundNodeList, function(backgroundNode, index) {
+        var rule = '{background-image: url('+ lowbandwidthImageList[index] +');}';
+        var selectors = backgroundNode.selectors.map(function(selector) {
+          return '.lowbandwidth ' + selector;
+        });
+
+        root.nodes.splice(
+          root.nodes.indexOf(backgroundNode) + 1,
+          0,
+          postcss.parse(selectors + rule).first
+        );
+      });
 
       try {
         context.contents = new Buffer(root.toResult().css);
