f86427ffc3948beb6578f53f31015ef2daddfec1
anhulife <anhulife@gmail.com>
2016-02-25 16:45:45 +0800

    更改低带宽模式的图片处理，改成降低质量

--- test/test.js
+++ test/test.js
@@ -30,10 +30,10 @@ describe('miaow-lowbandwidth-task', function() {
   });
 
   it('修改样式', function() {
-    assert.equal(log.modules['foo.css'].destHash, '38b747114e77681cac428dc166c2e6b4');
+    assert.equal(log.modules['foo.css'].destHash, '3b8d4e24cb1068505026b6993aa9d1d3');
   });
 
   it('生成低流量图片', function() {
-    assert.equal(log.modules['foo.lowbandwidth.png'].destHash, 'f4e73883ae35fd2d63ec352cf222e74d');
+    assert.equal(log.modules['foo.lowbandwidth.png'].destHash, 'c05a575455479ebc2577fd5cb7a9de7e');
   });
 });
